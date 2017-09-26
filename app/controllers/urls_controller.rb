13fb834a827ecebfdfc1e1b40576d54b74553025
Alex <forkit4449@gmail.com>
2017-09-26 16:57:56 -0400

    completed

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,25 @@
+class UrlsController < ApplicationController
+  def create
+    url = Url.new(url_params)
+    if  url.save!
+      url.update!(short_url: SecureRandom.base64(5).split("=").first)
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  def show
+    debugger
+@url = Url.find_by_short_url(params[:id])
+redirect_to  @url.long_url
+  end
+
+  def index
+    @url = Url.new
+    @urls = Url.all
+  end
+  private
+  def url_params
+  return  params.require(:url).permit(:long_url)
+  end
+end
---
+++ app/models/url.rb
@@ -0,0 +1,5 @@
+class Url < ApplicationRecord
+  def to_param
+    self.short_url
+  end
+end
